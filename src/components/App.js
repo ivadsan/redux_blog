0e9a90cff63fd82d3680807ffe76b78a42211d2b
Iván D. Sánchez <cosmosoft.soluciones@gmail.com>
2020-04-12 00:04:14 -0500

    New deploy

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,26 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Menu from './Menu';
 import Usuarios from './Usuarios';
 import Tareas from './Tareas';
 import Publicaciones from './Publicaciones';
 import TareasGuardar from './Tareas/Guardar';
 
-
 const App = () => (
   <BrowserRouter>
     <Menu />
     <div id='container'>
-      <Route exact path='/' component={Usuarios} />
-      <Route exact path='/tareas' component={Tareas} />
-      <Route exact path='/tareas/guardar' component={TareasGuardar} />
-      <Route exact path='/tareas/guardar/:usu_id/:tar_id' component={TareasGuardar} />
-      <Route exact path='/publicaciones/:key' component={Publicaciones} />
+      <Switch>
+        <Route exact path='/' component={Usuarios} />
+        <Route exact path='/tareas' component={Tareas} />
+        <Route exact path='/tareas/guardar' component={TareasGuardar} />
+        <Route
+          exact
+          path='/tareas/guardar/:usu_id/:tar_id'
+          component={TareasGuardar}
+        />
+        <Route exact path='/publicaciones/:key' component={Publicaciones} />
+      </Switch>
     </div>
   </BrowserRouter>
 );
